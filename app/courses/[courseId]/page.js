0ec51d8278520548e105b344d7e51db79193db76
Demian Cordova <demiancordova@cordovaibe.com>
2026-05-11 20:48:26 -0500

    Lol

--- app/courses/[courseId]/page.js
+++ app/courses/[courseId]/page.js
@@ -207,7 +207,7 @@ export default function CoursePage() {
                         {maxLevels > 0 && (
                           <svg
                             width="120" height="120"
-                            className="absolute inset-0 -translate-x-4 -translate-y-2.5"
+                            className="absolute inset-0 -translate-x-3.5 -translate-y-2.75"
                             style={{
                               pointerEvents: 'none',
                             }}
